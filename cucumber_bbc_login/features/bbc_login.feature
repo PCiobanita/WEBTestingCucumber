312dad21d0d795f141aa4181d03418888bbea564
PaulCiobanita <pciobanita@spartaglobal.com>
2019-02-07 17:08:19 +0000

    created scenarions for password with only numbers, only letters and run the autometion to retrive the errors

--- cucumber_bbc_login/features/bbc_login.feature
+++ cucumber_bbc_login/features/bbc_login.feature
@@ -13,3 +13,45 @@ Feature: BBC Login
     And I input a password that is to short
     When I try to login
     Then I recieve error for password beeing to short
+
+  Scenario: Inputing an incorrect email
+    Given I access the bbc login page
+    And I input incorrect email
+    And I input incorrect password details
+    When I try to login
+    Then I recieve error not beeing valid email
+
+  Scenario: Inputing the wrong apssword for email
+    Given I access the bbc login page
+    And I input correct email
+    And I input incorrect password details
+    When I try to login
+    Then I recieve error beeing the wrong password for email
+
+  Scenario: Inputing a passowrd that only have letters
+    Given I access the bbc login page
+    And I input incorrect email
+    And I input letters only password
+    When I try to login
+    Then I recieve error beeing asking me to instert something other that letters
+
+  Scenario: Inputing a passowrd that only have numbers
+    Given I access the bbc login page
+    And I input incorrect email
+    And I input numbers only password
+    When I try to login
+    Then I recieve error beeing asking me to instert something other than numbers
+
+  Scenario: Inputing a passowrd that only have numbers
+    Given I access the bbc login page
+    And I input incorrect email
+    And I input passowrd with to many characters
+    When I try to login
+    Then I recieve error beeing telling me password to long
+
+  Scenario: Inputing a username to short
+    Given I access the bbc login page
+    And I input short username
+    And I input incorrect password details
+    When I try to login
+    Then I recieve error with username beeing to short
--- cucumber_bbc_login/features/step_definitions/bbc_login_step_defs.rb
+++ cucumber_bbc_login/features/step_definitions/bbc_login_step_defs.rb
@@ -2,7 +2,6 @@ Given("I access the bbc login page") do
   @bbc_site = BbcSite.new
   @bbc_site.bbc_homepage.visit_homepage
   @bbc_site.bbc_homepage.select_signin
-  sleep 2
 end
 
 Given("I input incorrect username details") do
@@ -15,7 +14,6 @@ end
 
 When("I try to login") do
   @bbc_site.bbc_signin.select_signin
-  sleep 2
 end
 
 Then("I recieve error for not finding the account") do
@@ -34,3 +32,41 @@ Then("I recieve error for password beeing to short") do
   expect(@bbc_site.bbc_signin.get_password_error_messege).to eq @bbc_site.bbc_signin.error_password
 end
 
+
+# inputing invalid email
+Given("I input incorrect email") do
+  @bbc_site.bbc_signin.fillin_username('apple1231@btint')
+end
+
+Then("I recieve error not beeing valid email") do
+  expect(@bbc_site.bbc_signin.get_error_messege).to eq @bbc_site.bbc_signin.error_invalid_email
+end
+
+
+#inputing wrong password
+# Given("I input correct email") do
+#   @bbc_site.bbc_signin.fillin_username('paul.ciobanita@gmail.com')
+# end
+
+# Then("I recieve error beeing the wrong password for email") do
+#   expect(@bbc_site.bbc_signin.get_password_error_messege).to eq @bbc_site.bbc_signin.error_password | @bbc_site.bbc_signin.error_password_no_numbers
+# end
+
+# inputng password with only letters
+Given("I input letters only password") do
+  @bbc_site.bbc_signin.fillin_password('helloqwww')
+end
+
+Then("I recieve error beeing asking me to instert something other that letters") do
+  expect(@bbc_site.bbc_signin.get_password_error_messege).to eq @bbc_site.bbc_signin.error_password_no_numbers
+end
+
+
+#inputing numbers only passord
+Given("I input numbers only password") do
+  @bbc_site.bbc_signin.fillin_password('12345225422')
+end
+
+Then("I recieve error beeing asking me to instert something other than numbers") do
+  expect(@bbc_site.bbc_signin.get_password_error_messege).to eq @bbc_site.bbc_signin.error_password_no_letters
+end
